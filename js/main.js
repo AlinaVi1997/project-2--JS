41170a57dbbfb5f7dac4708a2350fdefa981e2f2
Alina <alinavyazovtseva@gmail.com>
2023-06-03 13:38:23 +0300

    project

--- js/main.js
+++ js/main.js
@@ -1,23 +1,81 @@
-var swiper = new Swiper('.image-slider',{
-    // autoplay: {
-    //     delay: 5000,
-    // },
+// //btn-gallery
+function showImages() {
+  var button = document.querySelector('button');
+  var imageContainer = document.getElementById('image-container');
+
+  button.style.display = 'block';
+  imageContainer.style.display = 'grid'; 
+}
+
+//print text
+var text = "SIMPLE & MODERN";
+var delay = 100; 
+var elem = document.getElementById("result");
+ 
+var print_text = function(text, elem, delay) {
+    if(text.length > 0) {
+        elem.innerHTML += text[0];
+        setTimeout(
+            function() {
+                print_text(text.slice(1), elem, delay); 
+            }, delay
+        );
+    }
+}
+print_text(text, elem, delay);
+
+//swiper-slide-top
+var swiper = new Swiper('.image-slider', {
+    autoplay: {
+        delay: 2000,
+    },
     speed: 800,
     direction: 'vertical',
-    freeMode: true,
-    autoHeight: true,
     slidesPerView: 1,
+    loop: true,
+    simulateTouch: false,
+    touchRotation: 0,
+    touchAngle: 0,
     pagination: {
-        el: '.swiper-pagination',
+        el: '.pag-1',
         clickable: true, 
     },
 });
 
-    // navigator: {
-    //     nextEl: '.swiper-button-next',
-    //     prevEl: '.swiper-button-prev',
-    // },
+//swiper-slider-news
+var swiper1 = new Swiper('.swiper-container-2', {
+    speed: 800,
+    freeMode: true,
+    autoHeight: true,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: '.pag-2',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        798: {
+            slidesPerView: 2,
+        },
+        1920: {
+            slidesPerView: 3,
+        }
+    },
+});
 
+//gallery
+Fancybox.bind('[data-fancybox]', {
+});
+
+//map
 function initMap() {
   const myLatLng = { lat: 41.12118893425201, lng: -73.43283741633479}
   const map = new google.maps.Map(document.getElementById("map"), {
@@ -35,3 +93,11 @@ function initMap() {
   });
 }
 window.initMap = initMap;
+
+//burder-menu
+const menuBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('.header__menu');
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('menu--active');
+});
